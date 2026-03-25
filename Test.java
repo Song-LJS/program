5565605b484131f1c2bb851f648c6c80755f8142
Song-LJS <3510889018@qq.com>
2026-03-25 23:18:24 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,31 +1,15 @@
-package day19and20;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
+package day14;
 
 public class Test {
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame();
-        jFrame.setSize(603,680);
-        jFrame.setTitle("事件演示");
-        jFrame.setAlwaysOnTop(true);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setDefaultCloseOperation(3);
-        jFrame.setLayout(null);
+        Huskly d1 = new Huskly();
+        d1.eat();
+        d1.breakhome();
 
-        JButton jtb = new JButton("点我");
-        jtb.setBounds(0, 0, 100, 50);
+        Sapi d2 = new Sapi();
+        d2.eat();
 
-        jtb.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                System.out.println("点我干啥");
-            }
-        });
-
-        jFrame.getContentPane().add(jtb);
-
-        jFrame.setVisible(true);
+        China d3 = new China();
+        d3.eat();
     }
 }
