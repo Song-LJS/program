1168cbcfe1be80c77e5bde066b4f673a880c005d
Song-LJS <3510889018@qq.com>
2026-03-30 22:36:13 +0800

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,12 +1,48 @@
-package day18;
+package day24;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
-        frog f = new frog(1,"song");
-        f.eat();
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (true) { 
+            System.out.println("输入一个整数：");
+            String str = sc.nextLine();
+            if(!fun(str)){
+                System.out.println("数字范围不再1~100，请重新输入：");
+                continue;
+            }else{
+                list.add(Integer.valueOf(str));
+            }
 
-        Dog d = new Dog(2,"li");
-        d.eat();
-        d.drink();
+            if(!result(list)){
+                break;
+            }
+        }
+
+        for(int i = 0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static boolean fun(String str){
+        int num = Integer.parseInt(str);
+        if(num>=1 && num<=100){
+            return true;
+        }
+        return false;
+    } 
+
+    public static boolean result(ArrayList<Integer> list){
+        int sum = 0;
+        for(int i = 0;i<list.size();i++){
+            sum+=list.get(i);
+        }
+        if(sum>200){
+            return false;
+        }
+        return true;
     }
 }
